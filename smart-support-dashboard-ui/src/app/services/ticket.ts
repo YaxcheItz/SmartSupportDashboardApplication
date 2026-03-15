bdc113f9bd017e06a8ed99a01485046c104e51b1
YaxcheItz <yaxtibla1@gmail.com>
2026-03-14 19:43:28 -0600

    🚀 Dep: Deploy first try

--- smart-support-dashboard-ui/src/app/services/ticket.ts
+++ smart-support-dashboard-ui/src/app/services/ticket.ts
@@ -9,7 +9,7 @@ import { Ticket } from '../models/ticket.model'; // Importamos nuestro molde
 })
 export class TicketService {
   // La URL de tu servidor Java
-  private apiUrl = 'https://smart-support-dashboard.onrender.com';
+  private apiUrl = 'https://smart-support-dashboard.onrender.com/api/tickets';
 
   // Inyectamos el "Postman" de Angular
   private http = inject(HttpClient);
