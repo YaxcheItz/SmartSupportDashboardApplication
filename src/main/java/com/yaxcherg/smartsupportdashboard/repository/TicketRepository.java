b6ba6b05b0cc7730cd2f6579dad3c8513f33cc12
YaxcheItz <yaxtibla1@gmail.com>
2026-03-17 11:37:17 -0600

    🔧 Fix: Extend JpaSpecificationExecutor in TicketRepository to enable filtering

--- src/main/java/com/yaxcherg/smartsupportdashboard/repository/TicketRepository.java
+++ src/main/java/com/yaxcherg/smartsupportdashboard/repository/TicketRepository.java
@@ -5,9 +5,10 @@ import com.yaxcherg.smartsupportdashboard.model.Ticket;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
+public interface TicketRepository extends JpaRepository<Ticket, Long>, JpaSpecificationExecutor<Ticket> {
     Page<Ticket> findByCreatedBy(AppUser createdBy, Pageable pageable);
 }
